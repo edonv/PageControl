4c27480e0dfa3881deb96a7ef2c1502119d50840
Edon <edon@valdman.works>
2026-05-13 12:21:27 -0500

    Create PageControl.swift
    Started with Apple's code.

--- Sources/PageControl/PageControl.swift
+++ Sources/PageControl/PageControl.swift
@@ -1,2 +1,48 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  PageControl.swift
+//  PageControl
+//
+//  Created by Edon Valdman on 5/13/26.
+//  Starting point created by Apple
+//  https://developer.apple.com/tutorials/swiftui/interfacing-with-uikit
+//
+
+import SwiftUI
+import UIKit
+
+struct PageControl: UIViewRepresentable {
+    var numberOfPages: Int
+    @Binding var currentPage: Int
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIView(context: Context) -> UIPageControl {
+        let control = UIPageControl()
+        control.numberOfPages = numberOfPages
+        control.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.updateCurrentPage(sender:)),
+            for: .valueChanged)
+        
+        return control
+    }
+    
+    func updateUIView(_ uiView: UIPageControl, context: Context) {
+        uiView.currentPage = currentPage
+    }
+    
+    class Coordinator: NSObject {
+        var control: PageControl
+        
+        init(_ control: PageControl) {
+            self.control = control
+        }
+        
+        @objc
+        func updateCurrentPage(sender: UIPageControl) {
+            control.currentPage = sender.currentPage
+        }
+    }
+}
