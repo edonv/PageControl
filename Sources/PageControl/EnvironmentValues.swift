6a614f25ca0e5361d22b99502336d1efee9b3352
Edon <edon@valdman.works>
2026-05-13 12:42:39 -0500

    Update EnvironmentValues.swift
    Added `.pageControlDirection()` view modifier via `Environment`.

--- Sources/PageControl/EnvironmentValues.swift
+++ Sources/PageControl/EnvironmentValues.swift
@@ -16,6 +16,13 @@ extension EnvironmentValues {
     /// Assign `false` (the default) to show the page control if there is only one page.
     @Entry
     public var pageControlHidesForSinglePage: Bool = false
+    
+    /// The layout direction of the page indicators.
+    ///
+    /// The default value is [`UIPageControl.Direction.natural`](https://developer.apple.com/documentation/uikit/uipagecontrol/direction-swift.enum/natural).
+    @Entry
+    @available(iOS 16.0, macCatalyst 16.0, tvOS 16.0, visionOS 1.0, *)
+    public var pageControlDirection: PageControl.Direction = .natural
 }
 
 // MARK: - ViewModifiers
@@ -28,5 +35,14 @@ extension View {
     nonisolated
     public func pageControlHidesForSinglePage(_ hide: Bool = true) -> some View {
         environment(\.pageControlHidesForSinglePage, hide)
+    }
+    
+    /// The layout direction of the page indicators.
+    ///
+    /// The default value is [`UIPageControl.Direction.natural`](https://developer.apple.com/documentation/uikit/uipagecontrol/direction-swift.enum/natural).
+    @available(iOS 16.0, macCatalyst 16.0, tvOS 16.0, visionOS 1.0, *)
+    nonisolated
+    public func pageControlDirection(_ direction: PageControl.Direction) -> some View {
+        environment(\.pageControlDirection, direction)
     }
 }
