0f96722914d4074ee610eeba5b1afdad4714867a
Edon <edon@valdman.works>
2026-05-13 13:52:29 -0500

    Update PageControl.swift
    Reordered `#available` `if` statements in order of OS version.

--- Sources/PageControl/PageControl.swift
+++ Sources/PageControl/PageControl.swift
@@ -41,16 +41,19 @@ public struct PageControl: UIViewRepresentable {
         
         // Custom EnvironmentValues
         control.hidesForSinglePage = context.environment.pageControlHidesForSinglePage
-        if #available(iOS 16.0, macCatalyst 16.0, tvOS 16.0, visionOS 1.0, *) {
-            control.direction = context.environment.pageControlDirection
+        
+        if #available(iOS 13.0, macCatalyst 13.1, tvOS 13.0, visionOS 1.0, *) {
+            control.pageIndicatorTintColor = context.environment.pageControlPageIndicatorTint
+            control.currentPageIndicatorTintColor = context.environment.pageControlCurrentPageIndicatorTint
         }
+        
         if #available(iOS 14.0, macCatalyst 14.0, tvOS 14.0, visionOS 1.0, *) {
             control.backgroundStyle = context.environment.pageControlBackgroundStyle
             control.allowsContinuousInteraction = !context.environment.pageControlContinuousInteractionDisabled
         }
-        if #available(iOS 13.0, macCatalyst 13.1, tvOS 13.0, visionOS 1.0, *) {
-            control.pageIndicatorTintColor = context.environment.pageControlPageIndicatorTint
-            control.currentPageIndicatorTintColor = context.environment.pageControlCurrentPageIndicatorTint
+        
+        if #available(iOS 16.0, macCatalyst 16.0, tvOS 16.0, visionOS 1.0, *) {
+            control.direction = context.environment.pageControlDirection
         }
         
         return control
