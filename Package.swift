4e211ee40a6477083dca1765fef7647ab9e9edd5
Edon <edon@valdman.works>
2026-05-13 11:22:48 -0500

    Update Package.swift
    Added supported platforms.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "PageControl",
+    platforms: [
+        .iOS(.v13),
+        .macCatalyst("13.1"),
+        .tvOS(.v13),
+        .visionOS(.v1),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
