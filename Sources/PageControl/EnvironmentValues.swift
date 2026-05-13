c0d59128aeef6a0aa433dcb89f6ce18f46fc5787
Edon <edon@valdman.works>
2026-05-13 12:48:59 -0500

    Update EnvironmentValues.swift
    Added `.pageControlBackgroundStyle()` view modifier via `Environment`.

--- Sources/PageControl/EnvironmentValues.swift
+++ Sources/PageControl/EnvironmentValues.swift
@@ -23,6 +23,11 @@ extension EnvironmentValues {
     @Entry
     @available(iOS 16.0, macCatalyst 16.0, tvOS 16.0, visionOS 1.0, *)
     public var pageControlDirection: PageControl.Direction = .natural
+    
+    /// The preferred background style for a ``PageControl/PageControl``.
+    @Entry
+    @available(iOS 14.0, macCatalyst 14.0, tvOS 14.0, visionOS 1.0, *)
+    public var pageControlBackgroundStyle: PageControl.BackgroundStyle = .automatic
 }
 
 // MARK: - ViewModifiers
@@ -44,5 +49,14 @@ extension View {
     nonisolated
     public func pageControlDirection(_ direction: PageControl.Direction) -> some View {
         environment(\.pageControlDirection, direction)
+    }
+    
+    /// The preferred background style for a ``PageControl/PageControl``.
+    ///
+    /// The default value is [`UIPageControl.BackgroundStyle.automatic`](https://developer.apple.com/documentation/uikit/uipagecontrol/backgroundstyle-swift.enum/automatic).
+    @available(iOS 14.0, macCatalyst 14.0, tvOS 14.0, visionOS 1.0, *)
+    nonisolated
+    public func pageControlBackgroundStyle(_ style: PageControl.BackgroundStyle) -> some View {
+        environment(\.pageControlBackgroundStyle, style)
     }
 }
