23a9de43f17e13cb5ae434975e1fd24623b2a8ce
Edon <edon@valdman.works>
2026-05-13 12:24:21 -0500

    Update PageControl.swift
    Indentation.

--- Sources/PageControl/PageControl.swift
+++ Sources/PageControl/PageControl.swift
@@ -29,7 +29,8 @@ public struct PageControl: UIViewRepresentable {
         control.addTarget(
             context.coordinator,
             action: #selector(Coordinator.updateCurrentPage(sender:)),
-            for: .valueChanged)
+            for: .valueChanged
+        )
         
         return control
     }
